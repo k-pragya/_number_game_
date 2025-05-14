e24111837cb29d601df8e41d509aad24e83070ec
Kumari Pragya <132121840+k-pragya@users.noreply.github.com>
2025-05-14 23:05:21 +0530

    Update NumberGame

--- NumberGame.java
+++ NumberGame.java
@@ -1,12 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author user
- */
-public class NumberGame {
-    
+import java.util.*;
+public class number
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int chances=8;
+	int finals=0;
+	boolean playAgain=true;
+	System.out.println("Welcome!");
+	System.out.println("you have about "+chances+" to win the game");
+	while(playAgain){
+	    int rand=getrandN(1,100);
+	    boolean guess=false;
+	    for(int i=0;i<chances;i++){
+	        System.out.println("chances "+(i+1)+" enter your guess:");
+	        int user=sc.nextInt();
+	        if(user==rand){
+	            guess=true;
+	            finals+=1;
+	            System.out.println("You won it!");
+	            break;
+	        }
+	        else if(user>rand){
+	            System.out.println("Too High!");
+	        }
+	        else {
+	            System.out.println("Too Low!");
+	        }
+	        
+	    }
+	    if(guess==false){
+	        System.out.println("Sorry you lost your chances. the number is " +rand);
+	    }
+	    System.out.println("Do you want to continue(y/n)");
+	    String pA=sc.next();
+	    playAgain=pA.equalsIgnoreCase("y");
+	    }
+	    System.out.println("This is your score "+finals);
+	}
+	    public static int getrandN(int min,int max){
+	        return (int)(Math.random()*(max-min+1)+min);
+	    }
+	
 }
